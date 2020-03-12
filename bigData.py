d532fca5c10d5fe7161c2fc1716979859a915c91
sofia valente <valente3@uni.coventry.ac.uk>
2020-03-12 14:14:34 +0000

    visualization loops creating 8 maps, one for each model, per hour.Each figure represents 1h.

--- bigData.py
+++ bigData.py
@@ -4,52 +4,43 @@ from matplotlib import pyplot as plt
 import cartopy.crs as ccrs
 
 
-
-rootgrp = xr.open_dataset("bigData.nc")                                              #opening the .nc file
+rootgrp = xr.open_dataset("bigData.nc")                                           #opening the .nc file
 
 lat = np.array(rootgrp.variables['lat'])                                          #extract/copy the data of lat
 long = np.array(rootgrp.variables['lon'])                                         #extract/copy the data of lon
-hour = np.array(rootgrp.variables['hour'])                                       #extract/copy the data of time
-
-# models = [
-#     'emep_ozone',
-
-# ]
-
-emep_ozone = np.array(rootgrp.variables['emep_ozone'][:])
+hour = np.array(rootgrp.variables['hour'])                                        #extract/copy the data of time
 
 
+models = [                                                                        #extract data from models: call each model models['emep_ozone']
+    np.array(rootgrp.variables['emep_ozone']),
+    np.array(rootgrp.variables['chimere_ozone']),
+    np.array(rootgrp.variables['ensemble_ozone']),              
+    np.array(rootgrp.variables['eurad_ozone']),
+    np.array(rootgrp.variables['lotoseuros_ozone']),
+    np.array(rootgrp.variables['match_ozone']),
+    np.array(rootgrp.variables['mocage_ozone']),
+    np.array(rootgrp.variables['silam_ozone']),
+]
 
-def avr():
-    hours = 0
-    values = 0
-    emep_ozoneTwoD = []
 
-    for i in range(len(hour)):
-        emep_ozoneTwoD = emep_ozone[i, :, :]
-        hours += 1
-        
-        for j in range(len(lat)):
-            for k in range(len(long)):
-                values += 1
-                print(emep_ozoneTwoD[j,k])
-                    
+#making graphs of O3 per hour
+def make_graph():
     
-    print('Hours: ', hours)
-    print(emep_ozone.size)
-    print(values)
-
-avr()
-
-#making graphs per hour
-def graph(array):
+    map_proj = ccrs.PlateCarree()
+    time = 0
 
     for i in range(len(hour)):
-        emep_ozoneTwoD = emep_ozone[i, :, :]  #slices array converting it into 2D array for each hour
-        plt.figure()
-        ax = plt.axes(projection=ccrs.PlateCarree())
-        plt.contour(long, lat, emep_ozoneTwoD, 1000, transform=ccrs.PlateCarree())
-        ax.coastlines(zorder=3)
-        ax.set_title(f'{i}h')
+        time += 1
+        for m in range(len(models)):
 
+            print('..............', m, '..............')
+
+            ax1 = plt.subplot(3, 3, (m + 1), projection = map_proj)
+            ax1.coastlines(zorder=3)
+            plt.contour(long, lat, models[m][(i+1), 1:, 1:], 1000, transform = ccrs.PlateCarree())
+            ax1.set_title(f'O3 levels in Europe in hour {i}')
+
+    print(time)
     plt.show()
+
+make_graph()
