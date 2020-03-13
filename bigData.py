bd95715441b1a97262f0d9d7bb5cd87d8e042db0
sofia valente <valente3@uni.coventry.ac.uk>
2020-03-13 11:51:33 +0000

    graph position and label change of grapgh position from 3x3 to 4x2 to make it more understandable label to understand O2 values (min, max)

--- bigData.py
+++ bigData.py
@@ -1,9 +1,15 @@
-import xarray as xr
-import numpy as np
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from matplotlib import pyplot as plt
+from joblib import Parallel, delayed
 import cartopy.crs as ccrs
+import multiprocessing as mp
+import numpy as np
+import xarray as xr
+import time as tm
 
 
+start = tm.perf_counter()
+num_cores = mp.cpu_count()
 rootgrp = xr.open_dataset("bigData.nc")                                           #opening the .nc file
 
 lat = np.array(rootgrp.variables['lat'])                                          #extract/copy the data of lat
@@ -22,6 +28,17 @@ models = [                                                                      
     np.array(rootgrp.variables['silam_ozone']),
 ]
 
+modelsNames = [                                                                        #extract data from models: call each model models['emep_ozone']
+    'emep_ozone',
+    'chimere_ozone',
+    'ensemble_ozone',
+    'eurad_ozone',
+    'lotoseuros_ozone',
+    'match_ozone',
+    'mocage_ozone',
+    'silam_ozone',
+]
+
 
 #making graphs of O3 per hour
 def make_graph():
@@ -31,16 +48,35 @@ def make_graph():
 
     for i in range(len(hour)):
         time += 1
+        print('Hour', time)
+        fig = plt.figure()
+
         for m in range(len(models)):
 
-            print('..............', m, '..............')
+            print('..............', (m + 1), '..............')
 
-            ax1 = plt.subplot(3, 3, (m + 1), projection = map_proj)
-            ax1.coastlines(zorder=3)
-            plt.contour(long, lat, models[m][(i+1), 1:, 1:], 1000, transform = ccrs.PlateCarree())
-            ax1.set_title(f'O3 levels in Europe in hour {i}')
+            ax1 = plt.subplot(4, 2, (m + 1), projection = map_proj)
+            ax1.coastlines(zorder = 3)
+            img = plt.contour(long, lat, models[m][i, :, :], 1000, transform = ccrs.PlateCarree())
+            plt.subplots_adjust(hspace = 0.4)
+            ax1.set_title(f'{modelsNames[m]}', fontsize = 9)
 
-    print(time)
-    plt.show()
+        axins = inset_axes(ax1,
+                    width = "7%",  
+                    height = "500%",  
+                    loc = 'lower left',
+                    bbox_to_anchor = (1.2, 0., 1, 1),
+                    bbox_transform = ax1.transAxes,
+                   )
+
+        plt.colorbar(img, cax = axins, orientation="vertical")
+        finish = tm.perf_counter()
+        print(f'Finished in {round(finish-start,2)} second(s)')
+        fig.suptitle(f'O3 levels in {i} hour(s)')
+        plt.show()
 
 make_graph()
+
+finish = tm.perf_counter()
+print(f'Finished in {round(finish-start,2)} second(s)')
+
